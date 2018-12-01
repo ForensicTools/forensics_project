a2860009055ff5107466ee66d8378da4b432118e
BillK7890 <truebluedrag@yahoo.com>
2018-12-01 16:08:46 -0500

    bug fix
    updated path discovery

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Peter\PycharmProjects\iBackupTool\venv"
+$env:VIRTUAL_ENV="C:\Users\trueb\Documents\GitHub\forensics_project\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
